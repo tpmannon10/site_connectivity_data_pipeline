bd97c8ff8ed7406b535aee0947f05dec8bc1b7cd
Timothy Mannon <timothy.mannon@powerflex.com>
2024-06-11 12:25:50 -0700

    Finished Event Trigger script

--- connectivity_alert_trigger.py
+++ connectivity_alert_trigger.py
@@ -16,15 +16,25 @@ def alert_filtering(alerts, filter):
             relevant_alerts["alerts"].append(item)
     return relevant_alerts
 
+# create filtered alerts file
+def filtered_alerts_file(relevant_alerts, out_file_name):
+    relevant_alerts["date_time"] = datetime.now().isoformat(timespec='seconds')
+    filename = out_file_name + '.json'
+    json_object = json.dumps(relevant_alerts, indent=4)
+    with open(filename, 'w') as outfile:
+        outfile.write(json_object)
+    return
+
 # take in latest alerts
 alerts = open_alerts('nc_alerts_update.json')
-print(json.dumps(alerts, indent= 4))
 
 # obtain alert filter
 alert_filter = open_alerts('alert_filter.json')
 
 # filter out relevant alerts
 relevant_alerts = alert_filtering(alerts, alert_filter)
-print(json.dumps(relevant_alerts, indent= 4))
+
+# output filtered alerts list in json file
+filtered_alerts_file(relevant_alerts, 'filtered_alerts')
 
 
