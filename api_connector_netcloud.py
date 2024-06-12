f27449689ffb6689c272c9ad317f3bacee7715d9
Timothy Mannon <timothy.mannon@powerflex.com>
2024-06-12 06:05:19 -0700

    small correction to netcloud api connector

--- api_connector_netcloud.py
+++ api_connector_netcloud.py
@@ -97,7 +97,7 @@ def payload_file(parsed_data, out_file_name):
 load_dotenv('secrets_nc.env')
 
 # import inputs
-input_file = json.load(open('inputs_nc.json'))
+input_file = json.load(open('app_configs.json'))
 input_dict = input_file["inputs_nc"]
 
 #Retrieve JSON parsing structure based on metric
