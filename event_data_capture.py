767ff0e931e09671e2cca5e68a05a2d901a6910d
Timothy Mannon <timothy.mannon@powerflex.com>
2024-06-12 07:17:23 -0700

    cleaned up event data capture script

--- event_data_capture.py
+++ event_data_capture.py
@@ -34,42 +34,9 @@ def grafana_api_inputs_file(input_dict, out_file_name):
         outfile.write(json_object)
     return
 
-# inputs
+# main
 def capture_event_data(event_dict, input_dict, addl_dict):
-
-# event_dict = {
-#         "created_at": "2024-06-11T17:58:24.249477+00:00",
-#         "created_at_timeuuid": "3274f130-281c-11ef-bcba-3674ac214bf2",
-#         "detected_at": "2024-06-11T17:58:09+00:00",
-#         "friendly_info": "The Internal 150M-D (SIM1) mdm on port Internal 1 has connected to the network.",
-#         "type": "modem_wan_connected",
-#         "router": "https://www.cradlepointecm.com/api/v2/routers/3659485/",
-#         "description": "pf-0119-03 Rydell Northridge 010-2190",
-#         "name": "IBR600C-41d",
-#         "state": "online",
-#         "state_updated_at": "2024-06-11T17:58:18.918550+00:00",
-#         "acn": "0119",
-#         "acc": "03"
-#     }
-
-# input_dict = {
-#         "metrics": "cradlepoint_rsrp",
-#         "parse_profile": "default",
-#         "acn": "n/a",
-#         "acc": "n/a",
-#         "time_start": "n/a",
-#         "time_end": "n/a",
-#         "step": "5m",
-#         "out_file": "grafana_metric_"
-#     }
-
-# addl_dict = {
-#         "step": "5m",
-#         "out_file": "grafana_metric_",
-#         "time_window_hrs": "24"
-#     }
-
-# convert unix time and produce time_start and time_end bounds
+    # convert unix time and produce time_start and time_end bounds
     input_dict["time_start"], input_dict["time_end"] = convert_to_unix_time(event_dict["detected_at"], addl_dict["time_window_hrs"])
     input_dict["acn"] = event_dict["acn"]
     input_dict["acc"] = event_dict["acc"]
